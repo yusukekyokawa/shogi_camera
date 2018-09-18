1018a7cdaf92ff3d52f9c5f8c8414f37628042ae
yusukekyokawa <medakasuisui598@gmail.com>
2018-09-18 21:09:40 +0900

    何もしなかった気がする

--- shogi_ban.py
+++ shogi_ban.py
@@ -3,11 +3,43 @@ import numpy as np
 import datetime
 import os
 
-PATH =  "./images/result"
+PATH =  "./images/result/"
+
+def detect_shogi_ban(path):  # 写真の中から将棋盤を見つける
+
+    #保存するディレクトリの作成
+    save_directory_name = create_save_directory()
+
+    #画像の2値化
+    src, th2 = image_to_binary(path)
+
+    #輪郭抽出
+    image, contours, hierarchy = cv2.findContours(th2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    #輪郭の中で最大面積の物を返す
+    max_area_counter = get_max_area_counter(contours)
+
+    shogi_ban = contours[max_area_counter]
+
+    #輪郭の近似
+    approx_shogi_ban = contour_approximater(shogi_ban, 1)
+
+    h, w, x, y = draw_ban_contours(approx_shogi_ban, src)
+
+    save_path = os.path.join(save_directory_name, "shogi_ban.jpg")
+
+    cv2.imwrite(save_path, src[y:y + h, x:x + w])
+    cv2.imshow('output', src)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
 
 def create_save_directory():
+    #ファイル名を日付にする
     made_time = datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S")#フォーマットの指定
     save_directory_name =os.path.join(PATH, str(made_time))
+    os.makedirs(save_directory_name)
     return save_directory_name
 
 
@@ -36,35 +68,22 @@ def get_max_area_counter(contours):
     return max_area_counter
 
 
-def draw_ban_contours(contours, max_area_counter, src):
-    shogi_ban = contours[max_area_counter]
+def draw_ban_contours(shogi_ban, src):
     x, y, w, h = cv2.boundingRect(shogi_ban)
     cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return h, w, x, y
 
+def contour_approximater(cnt, i):
+    approx_contours = []
+    #輪郭の周囲の長さを計算
+    arclen = cv2.arcLength(cnt, True)
+    epsilon = 0.01*arclen
+    approx_cnt = cv2.approxPolyDP(cnt, epsilon, closed=True)
+    #点の数の推移を求める
+    # print('contour {}:{} -> {}'.format(i, len(cnt), len(approx_cnt)))
+    return approx_cnt
 
-def detect_shogi_ban(path):  # 写真の中から将棋盤を見つける
 
-    #保存するディレクトリの作成
-    save_directory_name = create_save_directory()
-
-    #画像の2値化
-    src, th2 = image_to_binary(path)
-
-    #輪郭抽出
-    image, contours, hierarchy = cv2.findContours(th2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-    #輪郭の中で最大面積の物を返す
-    max_area_counter = get_max_area_counter(contours)
-
-    h, w, x, y = draw_ban_contours(contours, max_area_counter, src)
-
-    save_path = os.path.join(save_directory_name, "shogi_ban.jpg")
-    cv2.imwrite(save_path, src[y:y + h, x:x + w])
-    cv2.imshow('output', src)
-    cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
 
 
 
